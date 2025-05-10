a6463e86422a8505b60065dacc35affdbb391811
matteo.mahler <matteo.mahler@ost.ch>
2025-05-10 19:59:17 +0200

    cleanup

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+import plugin from 'tailwindcss/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      transitionProperty: {
+        'width': 'width'
+      },
+    },
+  },
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('touch-only', '@media (hover:none)')
+      addVariant('not-first', '&:not(:first-child)');
+    })
+  ]
+}
